f8e5bd27b5698c5a7867c4f6a631330439084760
JOSEPH TIAM <115112733+JOSEPHTIAM@users.noreply.github.com>
2022-11-18 18:32:20 +0100

    Add files via upload

--- tables_ingredients_repas.sql
+++ tables_ingredients_repas.sql
@@ -1,9 +1,60 @@
---
--- create table INGREDIENTS_REPAS by GROUPE 8
---
-
-CREATE TABLE INGREDIENTS_REPAS (
-  id_repas      number(10) NOT NULL, 
-  id_ingredient number(10) NOT NULL, 
-  quantite      number(10) PRIMARY KEY NOT NULL 
-);
+INSERT INTO INGREDIENTS_REPAS(id_repas,id_ingredient,quantite)VALUES(001,1,3);
+INSERT INTO INGREDIENTS_REPAS(id_repas,id_ingredient,quantite)VALUES(002,2,4);
+INSERT INTO INGREDIENTS_REPAS(id_repas,id_ingredient,quantite)VALUES(003,3,7);
+INSERT INTO INGREDIENTS_REPAS(id_repas,id_ingredient,quantite)VALUES(004,4,5);
+INSERT INTO INGREDIENTS_REPAS(id_repas,id_ingredient,quantite)VALUES(005,5,6);
+INSERT INTO INGREDIENTS_REPAS(id_repas,id_ingredient,quantite)VALUES(006,6,8);
+INSERT INTO INGREDIENTS_REPAS(id_repas,id_ingredient,quantite)VALUES(007,7,9);
+INSERT INTO INGREDIENTS_REPAS(id_repas,id_ingredient,quantite)VALUES(008,8,10);
+INSERT INTO INGREDIENTS_REPAS(id_repas,id_ingredient,quantite)VALUES(009,9,11);
+INSERT INTO INGREDIENTS_REPAS(id_repas,id_ingredient,quantite)VALUES(010,10,12);
+INSERT INTO INGREDIENTS_REPAS(id_repas,id_ingredient,quantite)VALUES(011,11,12);
+INSERT INTO INGREDIENTS_REPAS(id_repas,id_ingredient,quantite)VALUES(012,12,13);
+INSERT INTO INGREDIENTS_REPAS(id_repas,id_ingredient,quantite)VALUES(013,13,14);
+INSERT INTO INGREDIENTS_REPAS(id_repas,id_ingredient,quantite)VALUES(014,14,15);
+INSERT INTO INGREDIENTS_REPAS(id_repas,id_ingredient,quantite)VALUES(015,15,16);
+INSERT INTO INGREDIENTS_REPAS(id_repas,id_ingredient,quantite)VALUES(016,16,17);
+INSERT INTO INGREDIENTS_REPAS(id_repas,id_ingredient,quantite)VALUES(017,17,18);
+INSERT INTO INGREDIENTS_REPAS(id_repas,id_ingredient,quantite)VALUES(018,18,19);
+INSERT INTO INGREDIENTS_REPAS(id_repas,id_ingredient,quantite)VALUES(019,19,20);
+INSERT INTO INGREDIENTS_REPAS(id_repas,id_ingredient,quantite)VALUES(020,20,21);
+INSERT INTO INGREDIENTS_REPAS(id_repas,id_ingredient,quantite)VALUES(021,21,22);
+INSERT INTO INGREDIENTS_REPAS(id_repas,id_ingredient,quantite)VALUES(022,22,23);
+INSERT INTO INGREDIENTS_REPAS(id_repas,id_ingredient,quantite)VALUES(023,23,24);
+INSERT INTO INGREDIENTS_REPAS(id_repas,id_ingredient,quantite)VALUES(024,24,25);
+INSERT INTO INGREDIENTS_REPAS(id_repas,id_ingredient,quantite)VALUES(025,25,26);
+INSERT INTO INGREDIENTS_REPAS(id_repas,id_ingredient,quantite)VALUES(026,26,27);
+INSERT INTO INGREDIENTS_REPAS(id_repas,id_ingredient,quantite)VALUES(027,27,28);
+INSERT INTO INGREDIENTS_REPAS(id_repas,id_ingredient,quantite)VALUES(028,28,29);
+INSERT INTO INGREDIENTS_REPAS(id_repas,id_ingredient,quantite)VALUES(029,29,30);
+INSERT INTO INGREDIENTS_REPAS(id_repas,id_ingredient,quantite)VALUES(030,30,31);
+INSERT INTO INGREDIENTS_REPAS(id_repas,id_ingredient,quantite)VALUES(031,31,32);
+INSERT INTO INGREDIENTS_REPAS(id_repas,id_ingredient,quantite)VALUES(032,32,33);
+INSERT INTO INGREDIENTS_REPAS(id_repas,id_ingredient,quantite)VALUES(033,33,34);
+INSERT INTO INGREDIENTS_REPAS(id_repas,id_ingredient,quantite)VALUES(034,34,35);
+INSERT INTO INGREDIENTS_REPAS(id_repas,id_ingredient,quantite)VALUES(035,35,36);
+INSERT INTO INGREDIENTS_REPAS(id_repas,id_ingredient,quantite)VALUES(036,36,37);
+INSERT INTO INGREDIENTS_REPAS(id_repas,id_ingredient,quantite)VALUES(037,37,38);
+INSERT INTO INGREDIENTS_REPAS(id_repas,id_ingredient,quantite)VALUES(038,38,39);
+INSERT INTO INGREDIENTS_REPAS(id_repas,id_ingredient,quantite)VALUES(039,39,40);
+INSERT INTO INGREDIENTS_REPAS(id_repas,id_ingredient,quantite)VALUES(040,40,41);
+INSERT INTO INGREDIENTS_REPAS(id_repas,id_ingredient,quantite)VALUES(041,41,42);
+INSERT INTO INGREDIENTS_REPAS(id_repas,id_ingredient,quantite)VALUES(042,42,43);
+INSERT INTO INGREDIENTS_REPAS(id_repas,id_ingredient,quantite)VALUES(043,43,44);
+INSERT INTO INGREDIENTS_REPAS(id_repas,id_ingredient,quantite)VALUES(044,44,45);
+INSERT INTO INGREDIENTS_REPAS(id_repas,id_ingredient,quantite)VALUES(045,45,46);
+INSERT INTO INGREDIENTS_REPAS(id_repas,id_ingredient,quantite)VALUES(046,46,47);
+INSERT INTO INGREDIENTS_REPAS(id_repas,id_ingredient,quantite)VALUES(047,47,48);
+INSERT INTO INGREDIENTS_REPAS(id_repas,id_ingredient,quantite)VALUES(048,48,49);
+INSERT INTO INGREDIENTS_REPAS(id_repas,id_ingredient,quantite)VALUES(049,49,50);
+INSERT INTO INGREDIENTS_REPAS(id_repas,id_ingredient,quantite)VALUES(050,50,51);
+INSERT INTO INGREDIENTS_REPAS(id_repas,id_ingredient,quantite)VALUES(051,51,52);
+INSERT INTO INGREDIENTS_REPAS(id_repas,id_ingredient,quantite)VALUES(052,52,53);
+INSERT INTO INGREDIENTS_REPAS(id_repas,id_ingredient,quantite)VALUES(053,53,54);
+INSERT INTO INGREDIENTS_REPAS(id_repas,id_ingredient,quantite)VALUES(054,54,55);
+INSERT INTO INGREDIENTS_REPAS(id_repas,id_ingredient,quantite)VALUES(055,55,56);
+INSERT INTO INGREDIENTS_REPAS(id_repas,id_ingredient,quantite)VALUES(056,56,57);
+INSERT INTO INGREDIENTS_REPAS(id_repas,id_ingredient,quantite)VALUES(057,57,58);
+INSERT INTO INGREDIENTS_REPAS(id_repas,id_ingredient,quantite)VALUES(058,58,59);
+INSERT INTO INGREDIENTS_REPAS(id_repas,id_ingredient,quantite)VALUES(059,59,60);
+INSERT INTO INGREDIENTS_REPAS(id_repas,id_ingredient,quantite)VALUES(060,60,61);
