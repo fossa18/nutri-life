8a3074a30ee8965e8a2f0bd18eda4eda6e366872
Jordan-Tchantchou <114906204+Jordan-Tchantchou@users.noreply.github.com>
2022-11-18 17:53:14 +0100

    insertion de la table repas
    insertion de la table repas

--- tables_repas.sql
+++ tables_repas.sql
@@ -1,10 +1,60 @@
---
--- create table REPAS by GROUPE 8
---
-
-CREATE TABLE REPAS (
-  id_repas   NUMBER(10) PRIMARY KEY NOT NULL, 
-  nom_repas  VARCHAR2(255) NOT NULL, 
-  prix_repas FLOAT(10) NOT NULL, 
-  type_repas VARCHAR2(255) NOT NULL
-);
+INSERT INTO REPAS(id_repas,nom_repas,prix_repas,type_repas)VALUES(001,'Cereales sans sucre au lait ecreme a la chicoree et au jus mandarines',2000,'petit dejeuner');
+INSERT INTO REPAS(id_repas,nom_repas,prix_repas,type_repas)VALUES(002,'papilotte de poisson aux legumes avce ananas et du pain',2500,'dejeuner');
+INSERT INTO REPAS(id_repas,nom_repas,prix_repas,type_repas)VALUES(003,'poulet blanc au curry orrichiette fromage blanc du pain et eau',1500,'diner');
+INSERT INTO REPAS(id_repas,nom_repas,prix_repas,type_repas)VALUES(004,'portions de produit laitier avec les pattes aux saumons a huile de soja',2000,'petit dejeuner');
+INSERT INTO REPAS(id_repas,nom_repas,prix_repas,type_repas)VALUES(005,'legumineuse avec du pain  au cereales et au ton',1500,'dejeuner');
+INSERT INTO REPAS(id_repas,nom_repas,prix_repas,type_repas)VALUES(006,'une portion de viande ou poisson ou oeuf',2500,'diner');
+INSERT INTO REPAS(id_repas,nom_repas,prix_repas,type_repas)VALUES(007,'saumon au olive',1500,'petit dejeuner');
+INSERT INTO REPAS(id_repas,nom_repas,prix_repas,type_repas)VALUES(008,'pattes viande de boeuf avec eau',1500,'dejeuner');
+INSERT INTO REPAS(id_repas,nom_repas,prix_repas,type_repas)VALUES(009,'portion de legumes maquereau huile soja',2500,'diner');
+INSERT INTO REPAS(id_repas,nom_repas,prix_repas,type_repas)VALUES(010,'pain complet fromage blanc jus orange',1500,'petit dejeuner');
+INSERT INTO REPAS(id_repas,nom_repas,prix_repas,type_repas)VALUES(011,'riz complet haricot rouge viandes',1500,'dejeuner');
+INSERT INTO REPAS(id_repas,nom_repas,prix_repas,type_repas)VALUES(012,'portion de fruit et legumes',2500,'diner');
+INSERT INTO REPAS(id_repas,nom_repas,prix_repas,type_repas)VALUES(013,'lait entier pain complet',1000,'petit dejeuner');
+INSERT INTO REPAS(id_repas,nom_repas,prix_repas,type_repas)VALUES(014,'pattes viande de boeuf avec eau',1500,'dejeuner');
+INSERT INTO REPAS(id_repas,nom_repas,prix_repas,type_repas)VALUES(015,'portion de legumes maquereau huile soja',2500,'diner');
+INSERT INTO REPAS(id_repas,nom_repas,prix_repas,type_repas)VALUES(016,'pain oeuf lait ecreme soja',2000,'petit dejeuner');
+INSERT INTO REPAS(id_repas,nom_repas,prix_repas,type_repas)VALUES(017,'poissson blanc et fruit de mer salade',2000,'dejeuner');
+INSERT INTO REPAS(id_repas,nom_repas,prix_repas,type_repas)VALUES(018,'Mijote de poulet et poivrons aux agrumes avce du riz brun',2500,'diner');
+INSERT INTO REPAS(id_repas,nom_repas,prix_repas,type_repas)VALUES(019,'tasse de the pain complet beurre',1500,'petit dejeuner');
+INSERT INTO REPAS(id_repas,nom_repas,prix_repas,type_repas)VALUES(020,'fillet de poisson epinard aux raisins secs yaourt et fruit',2500,'dejeuner');
+INSERT INTO REPAS(id_repas,nom_repas,prix_repas,type_repas)VALUES(021,'riz complet portion de viande eau ',2500,'diner');
+INSERT INTO REPAS(id_repas,nom_repas,prix_repas,type_repas)VALUES(022,'pain ble fromage a la creme banane et lait',2000,'petit dejeuner');
+INSERT INTO REPAS(id_repas,nom_repas,prix_repas,type_repas)VALUES(023,'crudite et fromage avec un jus ananas',1500,'dejeuner');
+INSERT INTO REPAS(id_repas,nom_repas,prix_repas,type_repas)VALUES(024,'poulet maigre riz fruits eau',2500,'diner');
+INSERT INTO REPAS(id_repas,nom_repas,prix_repas,type_repas)VALUES(025,'fruit legumes pommes de terre et mais salade',1500,'petit dejeuner');
+INSERT INTO REPAS(id_repas,nom_repas,prix_repas,type_repas)VALUES(026,'poissons blancs fruit de mer jus de raisin',1000,'dejeuner');
+INSERT INTO REPAS(id_repas,nom_repas,prix_repas,type_repas)VALUES(027,'bouillon de legume lait ecreme au soja',1500,'diner');
+INSERT INTO REPAS(id_repas,nom_repas,prix_repas,type_repas)VALUES(028,'fromage pomme pain complet eau ',1500,'petit dejeuner');
+INSERT INTO REPAS(id_repas,nom_repas,prix_repas,type_repas)VALUES(029,'pain yaourt',500,'dejeuner');
+INSERT INTO REPAS(id_repas,nom_repas,prix_repas,type_repas)VALUES(030,'crudite yaourt miel',3000,'diner');
+INSERT INTO REPAS(id_repas,nom_repas,prix_repas,type_repas)VALUES(031,'tasse de the sans sucre au citron',500,'petit dejeuner');
+INSERT INTO REPAS(id_repas,nom_repas,prix_repas,type_repas)VALUES(032,'steak grille sans matiere grasse',1200,'dejeuner');
+INSERT INTO REPAS(id_repas,nom_repas,prix_repas,type_repas)VALUES(033,'deux oeufs cuits durs',500,'diner');
+INSERT INTO REPAS(id_repas,nom_repas,prix_repas,type_repas)VALUES(034,'cafe sans sucre au pain complet',500,'petit dejeuner');
+INSERT INTO REPAS(id_repas,nom_repas,prix_repas,type_repas)VALUES(035,'fromage blanc tisane sans sucre',700,'dejeuner');
+INSERT INTO REPAS(id_repas,nom_repas,prix_repas,type_repas)VALUES(036,'choux rouge a soupe de vinaigre',700,'diner');
+INSERT INTO REPAS(id_repas,nom_repas,prix_repas,type_repas)VALUES(037,'trache de pain  complet cafe de mangarine',1000,'petit dejeuner');
+INSERT INTO REPAS(id_repas,nom_repas,prix_repas,type_repas)VALUES(038,'laitue et tomate en salade jus de citron',2500,'dejeuner');
+INSERT INTO REPAS(id_repas,nom_repas,prix_repas,type_repas)VALUES(039,'potage fruit bio pain complet',2500,'diner');
+INSERT INTO REPAS(id_repas,nom_repas,prix_repas,type_repas)VALUES(040,'cafe sans sucre puree de carotte lait',1000,'petit dejeuner');
+INSERT INTO REPAS(id_repas,nom_repas,prix_repas,type_repas)VALUES(041,'saumon haricot vert fruit',2500,'dejeuner');
+INSERT INTO REPAS(id_repas,nom_repas,prix_repas,type_repas)VALUES(042,'the vert tranche de pains au sesame',1000,'diner');
+INSERT INTO REPAS(id_repas,nom_repas,prix_repas,type_repas)VALUES(043,'jambon de dinde carotte',2000,'petit dejeuner');
+INSERT INTO REPAS(id_repas,nom_repas,prix_repas,type_repas)VALUES(044,'salade de laitue amande huile',2500,'dejeuner');
+INSERT INTO REPAS(id_repas,nom_repas,prix_repas,type_repas)VALUES(045,'pommes cafe',1000,'diner');
+INSERT INTO REPAS(id_repas,nom_repas,prix_repas,type_repas)VALUES(046,'oeuf aux epinards verre de jus tranche de pain complet',1500,'petit dejeuner');
+INSERT INTO REPAS(id_repas,nom_repas,prix_repas,type_repas)VALUES(047,'poulet champignon tomate ',2500,'dejeuner');
+INSERT INTO REPAS(id_repas,nom_repas,prix_repas,type_repas)VALUES(048,'salade de fruit jus pasteque',1500,'diner');
+INSERT INTO REPAS(id_repas,nom_repas,prix_repas,type_repas)VALUES(049,'lait sans sucre pain complet',1000,'petit dejeuner');
+INSERT INTO REPAS(id_repas,nom_repas,prix_repas,type_repas)VALUES(050,'haricot cuit fromage rape pain jus naturel',2500,'dejeuner');
+INSERT INTO REPAS(id_repas,nom_repas,prix_repas,type_repas)VALUES(051,'riz brun au thon tomate hachee huile olive',2000,'diner');
+INSERT INTO REPAS(id_repas,nom_repas,prix_repas,type_repas)VALUES(052,'eau',500,'petit dejeuner');
+INSERT INTO REPAS(id_repas,nom_repas,prix_repas,type_repas)VALUES(053,'pain ble entier de jambon dinde et salade carotte',2500,'dejeuner');
+INSERT INTO REPAS(id_repas,nom_repas,prix_repas,type_repas)VALUES(054,'salade de poulet',2000,'diner');
+INSERT INTO REPAS(id_repas,nom_repas,prix_repas,type_repas)VALUES(055,'the sans sucre kiwi',1000,'petit dejeuner');
+INSERT INTO REPAS(id_repas,nom_repas,prix_repas,type_repas)VALUES(056,'omelette tomate tranche de pain',1000,'dejeuner');
+INSERT INTO REPAS(id_repas,nom_repas,prix_repas,type_repas)VALUES(057,'salade verte fillet de saumon trache de pain',2000,'diner');
+INSERT INTO REPAS(id_repas,nom_repas,prix_repas,type_repas)VALUES(058,'cafe sans sucre pain ble beurre',1000,'petit dejeuner');
+INSERT INTO REPAS(id_repas,nom_repas,prix_repas,type_repas)VALUES(059,'salade poulet epinards',2500,'dejeuner');
+INSERT INTO REPAS(id_repas,nom_repas,prix_repas,type_repas)VALUES(060,'viande pomme de terre',1500,'diner');
