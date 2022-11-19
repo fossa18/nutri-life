5ccd1b3015a261ba09ef4dffb3550dc403b70a23
Fossa18 <93674717+fossa18@users.noreply.github.com>
2022-11-19 09:00:12 +0100

    Add files via upload

--- tables_personnes.sql
+++ tables_personnes.sql
@@ -1,16 +1,30 @@
---
--- create table PERSONNES by GROUPE 8
---
-
-CREATE TABLE PERSONNES 
-(
-  id_personne     NUMBER(10) PRIMARY KEY NOT NULL, 
-  nom_personne    VARCHAR2(255) NOT NULL, 
-  prenom_personne VARCHAR2(255) NOT NULL, 
-  date_naissance  DATE NOT NULL, 
-  genre           CHAR(255) CHECK (genre IN('homme','femme')) NOT NULL, 
-  poids_en_kg     FLOAT(5) CHECK (poids_en_kg>89) NOT NULL,
-  taille_en_m     FLOAT(5) CHECK (taille_en_m > 1) NOT NULL,
-  type_corps      VARCHAR2(255) NOT NULL
-);
-
+insert into PERSONNES (id_personne, nom_personne, prenom_personne, date_naissance, genre, poids_en_kg, taille_en_m, type_corps) values (12, 'Alane', 'junior', '12/05/1999','homme', '90', '2','mesomorphe');
+insert into PERSONNES (id_personne, nom_personne, prenom_personne, date_naissance, genre, poids_en_kg, taille_en_m, type_corps) values (13, 'takougang', 'liza', '11/04/2000','femme', '92','3','endomorphe');
+insert into PERSONNES (id_personne, nom_personne, prenom_personne, date_naissance, genre, poids_en_kg, taille_en_m, type_corps) values (14, 'Mbah', 'jenny', '15/03/2003', 'femme', '91', '3', 'endomorphe');
+insert into PERSONNES (id_personne, nom_personne, prenom_personne, date_naissance, genre, poids_en_kg, taille_en_m, type_corps) values (15, 'Tiam', 'joseph', '18/03/1998', 'homme', '90', '3', 'ectmorphe');
+insert into PERSONNES (id_personne, nom_personne, prenom_personne, date_naissance, genre, poids_en_kg, taille_en_m, type_corps) values (16, 'bavoua', 'borel', '19/06/1997', 'homme', '95', '2', 'ectmorphe');
+insert into PERSONNES (id_personne, nom_personne, prenom_personne, date_naissance, genre, poids_en_kg, taille_en_m, type_corps) values (17, 'zebaze', 'ryan', '17/07/2001', 'homme', '96', '3', 'mesomorphe');
+insert into PERSONNES (id_personne, nom_personne, prenom_personne, date_naissance, genre, poids_en_kg, taille_en_m, type_corps) values (18, 'atsangou', 'darel', '05/09/1999', 'homme', '92', '3', 'ectmorphe');
+insert into PERSONNES (id_personne, nom_personne, prenom_personne, date_naissance, genre, poids_en_kg, taille_en_m, type_corps) values (19, 'nguiakam', 'angele', '12/05/2001','femme', '90', '2','mesomorphe');
+insert into PERSONNES (id_personne, nom_personne, prenom_personne, date_naissance, genre, poids_en_kg, taille_en_m, type_corps) values (20, 'manga', 'rebecca', '11/04/2000','femme', '92','3','endomorphe');
+insert into PERSONNES (id_personne, nom_personne, prenom_personne, date_naissance, genre, poids_en_kg, taille_en_m, type_corps) values (21, 'malonju', 'laure', '15/03/2003', 'femme', '91', '3', 'endomorphe');
+insert into PERSONNES (id_personne, nom_personne, prenom_personne, date_naissance, genre, poids_en_kg, taille_en_m, type_corps) values (22, 'motso', 'alex', '17/02/1990', 'femme', '90', '3', 'ectmorphe');
+insert into PERSONNES (id_personne, nom_personne, prenom_personne, date_naissance, genre, poids_en_kg, taille_en_m, type_corps) values (23, 'lontsi', 'brice', '19/03/1991', 'homme', '95', '2', 'ectmorphe');
+insert into PERSONNES (id_personne, nom_personne, prenom_personne, date_naissance, genre, poids_en_kg, taille_en_m, type_corps) values (24, 'kangue', 'loic', '17/04/2002', 'homme', '96', '3', 'mesomorphe');
+insert into PERSONNES (id_personne, nom_personne, prenom_personne, date_naissance, genre, poids_en_kg, taille_en_m, type_corps) values (25, 'josue', 'nelson', '05/05/1999', 'homme', '92', '3', 'ectmorphe');
+insert into PERSONNES (id_personne, nom_personne, prenom_personne, date_naissance, genre, poids_en_kg, taille_en_m, type_corps) values (26, 'fotso', 'junior', '12/06/1999','homme', '90', '2','mesomorphe');
+insert into PERSONNES (id_personne, nom_personne, prenom_personne, date_naissance, genre, poids_en_kg, taille_en_m, type_corps) values (27, 'makolo', 'liza', '11/07/2000','femme', '92','3','endomorphe');
+insert into PERSONNES (id_personne, nom_personne, prenom_personne, date_naissance, genre, poids_en_kg, taille_en_m, type_corps) values (28, 'tatiana', 'laure', '15/08/2003', 'femme', '91', '3', 'endomorphe');
+insert into PERSONNES (id_personne, nom_personne, prenom_personne, date_naissance, genre, poids_en_kg, taille_en_m, type_corps) values (29, 'toko', 'michel', '18/09/1998', 'homme', '90', '3', 'ectmorphe');
+insert into PERSONNES (id_personne, nom_personne, prenom_personne, date_naissance, genre, poids_en_kg, taille_en_m, type_corps) values (30, 'sianou', 'cabrel', '19/10/1997', 'homme', '95', '2', 'ectmorphe');
+insert into PERSONNES (id_personne, nom_personne, prenom_personne, date_naissance, genre, poids_en_kg, taille_en_m, type_corps) values (31, 'samy', 'bodio', '17/11/2001', 'homme', '97', '3', 'mesomorphe');
+insert into PERSONNES (id_personne, nom_personne, prenom_personne, date_naissance, genre, poids_en_kg, taille_en_m, type_corps) values (32, 'maxime', 'junior', '05/12/1999', 'homme', '92', '3', 'ectmorphe');
+insert into PERSONNES (id_personne, nom_personne, prenom_personne, date_naissance, genre, poids_en_kg, taille_en_m, type_corps) values (33, 'tanko', 'leila', '12/12/1999','femme', '98', '2','mesomorphe');
+insert into PERSONNES (id_personne, nom_personne, prenom_personne, date_naissance, genre, poids_en_kg, taille_en_m, type_corps) values (34, 'tchinda', 'yvana', '11/01/2000','femme', '99','3','endomorphe');
+insert into PERSONNES (id_personne, nom_personne, prenom_personne, date_naissance, genre, poids_en_kg, taille_en_m, type_corps) values (35, 'ndombe', 'lesly', '15/02/2003', 'femme', '91', '3', 'endomorphe');
+insert into PERSONNES (id_personne, nom_personne, prenom_personne, date_naissance, genre, poids_en_kg, taille_en_m, type_corps) values (36, 'tagomo', 'joseph', '18/04/1998', 'homme', '90', '3', 'endomorphe');
+insert into PERSONNES (id_personne, nom_personne, prenom_personne, date_naissance, genre, poids_en_kg, taille_en_m, type_corps) values (37, 'bavoua', 'borel', '19/06/1997', 'homme', '95', '2', 'ectmorphe');
+insert into PERSONNES (id_personne, nom_personne, prenom_personne, date_naissance, genre, poids_en_kg, taille_en_m, type_corps) values (38, 'tchepda', 'ornella', '17/07/2001', 'femme', '96', '3', 'mesomorphe');
+insert into PERSONNES (id_personne, nom_personne, prenom_personne, date_naissance, genre, poids_en_kg, taille_en_m, type_corps) values (39, 'ndjakop', 'stephy', '05/09/1999', 'femme', '92', '3','mesomorphe');
+insert into PERSONNES (id_personne, nom_personne, prenom_personne, date_naissance, genre, poids_en_kg, taille_en_m, type_corps) values (40, 'nguegang', 'stive', '05/09/1998', 'homme', '92', '2','ectmorphe');
+insert into PERSONNES (id_personne, nom_personne, prenom_personne, date_naissance, genre, poids_en_kg, taille_en_m, type_corps) values (41, 'nikiledji', 'kevine', '05/09/2000', 'homme', '92', '3','endomorphe');
