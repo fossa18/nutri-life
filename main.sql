6ab6fa36c7392fd23570461f58147316a8fa62c8
Fossa18 <93674717+fossa18@users.noreply.github.com>
2022-11-19 09:01:15 +0100

    Add files via upload

---
+++ main.sql
@@ -0,0 +1,54 @@
+ALTER SESSION SET NLS_LANGUAGE=English;
+ALTER SESSION SET NLS_TERRITORY=America;
+ALTER session set NLS_DATE_FORMAT='DD/MM/YYYY';
+--insert into tables
+
+
+
+PROMPT*************** insert into tables_utilisateurs****************************************
+@data/tables_utilisateurs
+
+PROMPT*************** insert into tables_personnes*******************************************
+@data/tables_personnes
+
+
+PROMPT*************** insert into tables_programmes_regimes***********************
+@data/tables_programmes_regimes
+
+
+PROMPT*************** insert into tables_activites********************************************
+@data/tables_activites
+
+
+PROMPT*************** insert into tables_repas***********************************************
+@data/tables_repas
+
+
+PROMPT*************** insert into tables_ingredients**********************
+@data/tables_ingredients
+
+
+PROMPT*************** insert into tables_souscriptions*********************
+@data/tables_souscriptions
+
+
+PROMPT*************** insert into tables_parametres_membres*************************
+@data/tables_parametres_membres
+
+
+PROMPT*************** insert into tables_regimes_activites*********************
+@data/tables_regimes_activites
+
+
+PROMPT*************** insert into tables_menus_repas*********************
+@data/tables_menus_repas
+
+
+PROMPT*************** insert into tables_ingredients_repas***********************************
+@data/tables_ingredients_repas
+
+
+
+
+
+
